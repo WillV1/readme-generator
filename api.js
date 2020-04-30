75d07b4a31781d03d79ef43b1ec9abad80538c72
WillV1 <wmvanhook@gmail.com>
2020-04-30 06:00:30 -0400

    Uploading current version of JS files

---
+++ api.js
@@ -0,0 +1,23 @@
+var axios = require("axios");
+var index = require("./utils/index.js");
+
+const api = {
+  getUser(username) {
+
+async function getEmail () {
+  try {
+    var email = await axios.get('https://api.github.com/users/' + username + '/events/public')
+      console.log(response.email);
+      var image = await axios.get('https://api.github.com/users/' + username + '/events/public')
+      console.log(response.avatar_url);
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+getEmail()    
+    
+  }
+};
+
+module.exports = api;
---
+++ index.js
@@ -0,0 +1,35 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+var path = require("path");
+
+const questions = [
+      {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+      },
+    //   {
+    //     type: "input",
+    //     message: "What is your GitHub e-mail address?",
+    //     name: "email"
+    //   }
+];
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(path.join(__dirname, fileName), data)
+
+}
+
+function init() {
+inquirer.prompt(questions)
+.then(data => {
+    console.log(data)
+writeToFile("readme.md", "GitHub")
+
+}) 
+
+}
+
+init();
+
+module.exports = index;
